e05df5e87ac06653d3a617825d3edd81e27a65f2
Gsromualdo <dalarius_romualdo@outlook.com>
2026-04-28 15:38:34 -0300

    Docker hub

--- src/test/java/br/com/gabrielromualdo/gestao_vagas/GestaoVagasApplicationTests.java
+++ src/test/java/br/com/gabrielromualdo/gestao_vagas/GestaoVagasApplicationTests.java
@@ -2,8 +2,10 @@ package br.com.gabrielromualdo.gestao_vagas;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class GestaoVagasApplicationTests {
 
 	@Test
