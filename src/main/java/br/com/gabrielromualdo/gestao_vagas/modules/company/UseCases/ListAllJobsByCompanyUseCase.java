c3d331788cca1c3a1b1fc91fb8c383aace2d7f5e
Gsromualdo <dalarius_romualdo@outlook.com>
2026-05-11 13:28:16 -0300

    listagem das vagas

--- src/main/java/br/com/gabrielromualdo/gestao_vagas/modules/company/UseCases/ListAllJobsByCompanyUseCase.java
+++ src/main/java/br/com/gabrielromualdo/gestao_vagas/modules/company/UseCases/ListAllJobsByCompanyUseCase.java
@@ -4,10 +4,12 @@ import java.util.List;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import br.com.gabrielromualdo.gestao_vagas.modules.company.entities.JobEntity;
 import br.com.gabrielromualdo.gestao_vagas.modules.company.repositories.JobRepository;
 
+@Service
 public class ListAllJobsByCompanyUseCase {
     
     @Autowired
